bfadefec95340ede265a4d306134fdc3a02a1adb
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-02-27 20:24:03 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,6 +10,17 @@ function setAPIKey(apikey) {
     key = apikey
 }
 
+/**
+    * Get all games.
+    *
+    * @return Promise<Object>
+    * 
+    * Returns an object containing an array called data.
+    * Data contains objects, representing each game.
+    * The objects contain something like shown in mod.io's official API docs: https://docs.mod.io/?javascript--nodejs#get-games-2
+
+*/
+
 function getGames() {
     // Send request to /v1/games endpoint to get all games.
     return fetch(`https://api.mod.io/v1/games?api_key=${key}`, {
@@ -19,6 +30,15 @@ function getGames() {
         }
     })
 }
+
+/**
+ * Gets a specific game.
+ * @param id ID of the game.
+ * @returns {*} Promise<Object>
+ * 
+ * Returns an object, containing multiple properties, including the game ID, status and others.
+ * You can see an example of it here: https://docs.mod.io/?javascript--nodejs#game-object
+ */
 
 function getGame(id) {
     // Send request to /v1/games endpoint with game ID to get details about the game.
@@ -30,6 +50,16 @@ function getGame(id) {
     })
 }
 
+/**
+ * Gets all the mods a game has.
+ * @param gameid ID of the game to get mods from.
+ * @returns {*} Promise<Object>
+ * 
+ * Contains an object, containing an array called data.
+ * This array contains multiple objects representing each mod.
+ * You can see an example here: https://docs.mod.io/?javascript--nodejs#get-mods-2
+ */
+
 function getMods(gameid) {
     // Send request to v1/games/{gameid}/mods with game ID to get mods for that game.
     return fetch(`https://api.mod.io/v1/games/${gameid}/mods?api_key=${key}`, {
@@ -39,6 +69,15 @@ function getMods(gameid) {
         }
     })
 }
+
+/**
+ * Gets a mod from a specific game.
+ * @param gameid The gameID to get the mod from.
+ * @param modid The ID of the mod to get.
+ * @returns {*} Promise<Object>
+ * Contains an object which contains a lot of properties.
+ * You can see an example here: https://docs.mod.io/?javascript--nodejs#mod-object
+ */
 
 function getMod(gameid, modid) {
     // Send request to v1/games/{gameid}/mods/{modid} to get info about the mod.
@@ -52,6 +91,15 @@ function getMod(gameid, modid) {
 
 // This only contains basic mod functions, no uploading, adding, editing or deleting yet.
 
+/**
+ * Gets the files of a specific mod.
+ * @param gameid Game to get files from.
+ * @param modid Mod to get files from.
+ * @returns {*} Promise<Object>
+ * Contains an object with an array.
+ * View example here: https://docs.mod.io/?javascript--nodejs#get-modfiles-2
+ */
+
 function getModfiles(gameid, modid) {
     // Send request to v1/games/{gameid}/mods/{modid}/files to get all files from the mod.
     return fetch(`https://api.mod.io/v1/games/${gameid}/mods/${modid}/files?api_key=${key}`, {
@@ -61,6 +109,18 @@ function getModfiles(gameid, modid) {
         }
     })
 }
+
+/**
+ * Gets a specific modfile.
+ * @param gameid Game to get mod from.
+ * @param modid Mod to get file from.
+ * @param platform Platform to get file for.
+ * @returns {*} Promise<Object>
+ * Contains an object.
+ * View example here: https://docs.mod.io/?javascript--nodejs#modfile-object
+ * Side note, I am not sure how to get the file id, meaning if there's more than one file for your platform, it will likely only download the first one it gets.
+ * If anyone knows how to get the file id, please do create a pull request.
+ */
 
 async function getModfile(gameid, modid, platform) {
     // This is supposed to send a request to v1/games/{gameid}/mods/{modid}/files/{fileid} to get a specific file from the mod, but I don't know where to get the fileid from.
@@ -75,6 +135,17 @@ async function getModfile(gameid, modid, platform) {
         }
     }
 }
+
+/**
+ * Downloads a mod.
+ * @param gameid Game to download mod from.
+ * @param modid Mod to download file from.
+ * @param platform Platform to download file for.
+ * @param outputpath Where to put the file.
+ * @returns {*} Promise
+ * 
+ * Downloads a mod based on it's platform ,because I don't know where to get the mod's fileid from.
+ */
 
 async function downloadMod(gameid, modid, platform, outputpath) {
     // This was supposed to send a request to v1/games/{gameid}/mods/{modid}/files/{fileid} to get a specific file from the mod, but I don't know where to get the fileid from.
@@ -94,6 +165,15 @@ async function downloadMod(gameid, modid, platform, outputpath) {
     return new Promise(resolve => {downloading.on('finish', resolve)}, reject => {downloading.on('error', (error) => reject(error))})
 }
 
+/**
+ * Gets the comments from a mod.
+ * @param gameid Game to get mod from.
+ * @param modid Mod to get comments from.
+ * @returns {*} Promise<Object>
+ * Object yet again contains an array called data.
+ * View example here: https://docs.mod.io/?javascript--nodejs#get-mod-comments-2
+ */
+
 function getModComments(gameid, modid) {
     // Send request to v1/games/{gameid}/mods/{modid}/comments to get comments from the mod.
     return fetch(`https://api.mod.io/v1/games/${gameid}/mods/${modid}/comments?api_key=${key}`, {
@@ -103,6 +183,15 @@ function getModComments(gameid, modid) {
         }
     })
 }
+
+/**
+ * Gets dependencies for a mod.
+ * @param gameid Game to get mod from.
+ * @param modid Mod to get dependencies for.
+ * @returns {*} Promise<Object>
+ * Object yet again contains an array called data.
+ * View example here: https://docs.mod.io/?javascript--nodejs#get-mod-dependencies-2
+ */
 
 function getModDependencies(gameid, modid) {
     // Send request to v1/games/{gameid}/mods/{modid}/dependencies to get mod dependencies.
