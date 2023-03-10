62fed3621aca8776c062379782a5e6af4b3ee040
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-03-10 15:02:33 +0200

    Create modfile.d.ts

---
+++ lib/modfile.d.ts
@@ -0,0 +1,20 @@
+export declare class Modfile {
+    filesize: number;
+    id: number;
+    mod_id: number;
+    date_added: number;
+    date_scanned: number;
+    virus_status: number;
+    virus_positive: number;
+    virustotal_hash: string;
+    md5: string;
+    filename: string;
+    version: string;
+    changelog: string;
+    metadata_blob: string;
+    binary_url: string;
+    date_expires: number;
+    platform: string;
+    constructor(tbl: any);
+    download(path: string): Promise<unknown>;
+}
