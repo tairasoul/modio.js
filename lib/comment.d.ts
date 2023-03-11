94edb80d9c0a30093160d5e2178507ffb0e98a64
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-03-11 12:37:03 +0200

    Update comment.d.ts

--- lib/comment.d.ts
+++ lib/comment.d.ts
@@ -1,16 +1,10 @@
+import * as interfaces from './interfaces';
 export declare class Comment {
     id: number;
     game_id: number;
     mod_id: number;
     resource_id: number;
-    user_id: number;
-    user_name_id: string;
-    username: string;
-    user_avatar_filename: string;
-    user_avatar_original: string;
-    user_avatar_50x50: string;
-    user_avatar_100x100: string;
-    user_profile_url: string;
+    user: interfaces.submitter;
     date_added: number;
     reply_id: number;
     thread_position: string;
