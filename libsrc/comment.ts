6da3f690122f15154089ec5b621d669d358ca79b
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-03-11 12:35:52 +0200

    now uses interface

--- libsrc/comment.ts
+++ libsrc/comment.ts
@@ -1,16 +1,11 @@
+import * as interfaces from './interfaces';
+
 export class Comment {
     id: number;
     game_id: number;
     mod_id: number;
     resource_id: number;
-    user_id: number;
-    user_name_id: string;
-    username: string;
-    user_avatar_filename: string;
-    user_avatar_original: string;
-    user_avatar_50x50: string;
-    user_avatar_100x100: string;
-    user_profile_url: string;
+    user: interfaces.submitter;
     date_added: number;
     reply_id: number;
     thread_position: string;
@@ -22,13 +17,13 @@ export class Comment {
         this.game_id = tbl.game_id;
         this.mod_id = tbl.mod_id;
         this.resource_id = tbl.resource_id;
-        this.user_id = tbl.user.id;
-        this.user_name_id = tbl.user.name_id;
-        this.username = tbl.user.username;
-        this.user_avatar_filename = tbl.user.avatar.filename;
-        this.user_avatar_original = tbl.user.avatar.original;
-        this.user_avatar_50x50 = tbl.user.avatar.thumb_50x50;
-        this.user_avatar_100x100 = tbl.user.avatar.thumb_50x50;
+        this.user.id = tbl.user.id;
+        this.user.name_id = tbl.user.name_id;
+        this.user.username = tbl.user.username;
+        this.user.avatar.filename = tbl.user.avatar.filename;
+        this.user.avatar.original = tbl.user.avatar.original;
+        this.user.avatar.thumb_50x50 = tbl.user.avatar.thumb_50x50;
+        this.user.avatar.thumb_100x100 = tbl.user.avatar.thumb_50x50;
         this.date_added = tbl.date_added;
         this.reply_id = tbl.reply_id;
         this.thread_position = tbl.thread_position;
