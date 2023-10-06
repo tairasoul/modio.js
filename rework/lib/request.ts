c6389d43b14e7536d1f053a0532cb7e3f665de86
taira <100938045+tairasoul@users.noreply.github.com>
2023-10-06 10:28:42 +0200

    base api.mod.io paths still work

--- rework/lib/request.ts
+++ rework/lib/request.ts
@@ -1,5 +1,5 @@
-export async function apiRequest(path: string, endpoint: RequestInfo | URL, args: RequestInit | undefined, testEnv: boolean = false) {
-    const data = await fetch(`${testEnv ? "https://api.test.mod.io/v1" : path}${endpoint}`, args);
+export async function apiRequest(endpoint: RequestInfo | URL, args: RequestInit | undefined, testEnv: boolean = false) {
+    const data = await fetch(`${testEnv ? "https://api.test.mod.io/v1" : "https://api.mod.io/v1"}${endpoint}`, args);
     const json = await data.json();
     return json;
 }
